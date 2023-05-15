5c17082225234efa60e906c0fc4c55f11f98c368
Matteo Nicoli <matteo.nicoli@polito.it>
2023-05-15 16:49:15 -0400

    Update database_creator.py
    This commit updates the database_creator.py including the deletion of the SQLite database at the beginning of the execution, if already existing, and printing possible errors during the SQL execution.

--- TEMOA-Italy/database_creator.py
+++ TEMOA-Italy/database_creator.py
@@ -1,12 +1,27 @@
+import os
 import sqlite3
 
-conn = sqlite3.connect('TEMOA_Italy.sqlite')
+database_name = 'TEMOA_Italy.sqlite'
+
+# Check if the SQLite database already exists and delete it
+
+if os.path.exists(database_name):
+    os.remove(database_name)
+    print('\nExisting SQLite database deleted.')
+
+# Create the SQLite database and execute the SQL code(s)
+
+conn = sqlite3.connect(database_name)
 with open('TEMOA_Italy.sql', 'r') as sql_file:
     conn.executescript(sql_file.read())
 conn.commit()
 conn.close()
 
-print('\nSQLite database created and filled according to the SQL code.\n')
+print('\nSQLite database created and SQL code executed.\n')
+
+# Execute the database_preprocessing.py script
 
 with open("database_preprocessing.py") as preprocessing:
     exec(preprocessing.read())
+
+print('\nSQLite database preprocessed.\n')
